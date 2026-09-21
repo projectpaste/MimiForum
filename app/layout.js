824d097955bda7e988669a6ca4c910f9e4f9b73e
4leaker <levzubov549@gmail.com>
2026-09-21 20:53:34 +0300

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -2,13 +2,20 @@ import './globals.css'
 
 export const metadata = {
   title: 'Mimi Forum 34',
-  description: 'Форум о Mimi',
+  description: 'Форум посвященный персонажу Mimi',
 }
 
 export default function RootLayout({ children }) {
   return (
     <html lang="ru">
-      <body>{children}</body>
+      <head>
+        <link rel="preconnect" href="https://googleapis.com" />
+        <link rel="preconnect" href="https://gstatic.com" crossOrigin="anonymous" />
+        <link href="https://googleapis.com/css2?family=JetBrains+Mono:wght@400;500;700&family=Unbounded:wght@400;700&display=swap" rel="stylesheet" />
+      </head>
+      <body className="bg-[#0b0c10] text-[#c5c6c7] font-mono selection:bg-[#1f2833] selection:text-[#66fcf1]">
+        {children}
+      </body>
     </html>
   )
 }
