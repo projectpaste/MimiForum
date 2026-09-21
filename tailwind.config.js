4f6805d0c7911ebf998806ef9f0a24a43e9bb280
4leaker <levzubov549@gmail.com>
2026-09-21 20:56:25 +0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,17 +2,11 @@
 module.exports = {
   content: [
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        mimi: {
-          pink: '#ff85a2',
-          purple: '#b388ff',
-          dark: '#1a1625'
-        }
-      }
-    },
+    extend: {},
   },
   plugins: [],
 }
